692475b0cd4a6e2842a4401d55545eaf085aa08b
RajkumarRj <rajpersonale@gmail.com>
2024-01-16 20:10:28 +0530

    updold files

---
+++ src/Header/Button.jsx
@@ -0,0 +1,9 @@
+const Button = () => {
+	return (
+		<button className='px-3 py-1.5 text-xs font-medium text-white duration-200 bg-blue-600 rounded-md md:text-sm hover:bg-blue-700'>
+			Get Started
+		</button>
+	)
+}
+
+export default Button
