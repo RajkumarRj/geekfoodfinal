09bbe9b3d5cd034e9e065e8cd83e73883134663e
RajkumarRj <rajpersonale@gmail.com>
2024-01-16 20:45:30 +0530

    update files

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,54 @@
-import About from './About/About'
-import Error from './Error'
-import Food from './Food/Food'
-import FoodItem from './Food/FoodItem'
-import Layout from './Layout'
-import Main from './Main/Main'
-import Quotes from './Quotes/Quotes'
-import Restaurants from './Restaurants/Restaurants'
+import React from 'react';
+import { createHashRouter, RouterProvider } from 'react-router-dom';
+import About from './About/About';
+import Error from './Error';
+import Food from './Food/Food';
+import FoodItem from './Food/FoodItem';
+import Layout from './Layout';
+import Main from './Main/Main';
+import Quotes from './Quotes/Quotes';
+import Restaurants from './Restaurants/Restaurants';
 
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-
-const router = createBrowserRouter([
-	{
-		path: '/',
-		element: <Layout />,
-		children: [
-			{
-				path: '/',
-				element: <Main />,
-			},
-			{
-				path: '/quote',
-				element: <Quotes />,
-			},
-			{
-				path: '/restaurant',
-				element: <Restaurants />,
-			},
-			{
-				path: '/food',
-				element: <Food />,
-			},
-			{
-				path: '/food/:id',
-				element: <FoodItem />,
-			},
-
-			{
-				path: '/contact',
-				element: <About />,
-			},
-		],
-		errorElement: <Error />,
-	},
-])
+const router = createHashRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      {
+        path: '/',
+        element: <Main />,
+      },
+      {
+        path: '/quote',
+        element: <Quotes />,
+      },
+      {
+        path: '/restaurant',
+        element: <Restaurants />,
+      },
+      {
+        path: '/food',
+        element: <Food />,
+      },
+      {
+        path: '/food/:id',
+        element: <FoodItem />,
+      },
+      {
+        path: '/contact',
+        element: <About />,
+      },
+    ],
+    errorElement: <Error />,
+  },
+]);
 
 const App = () => {
-	return (
-		<div className='relative'>
-			<RouterProvider router={router} />
-		</div>
-	)
-}
+  return (
+    <div className='relative'>
+      <RouterProvider router={router} />
+    </div>
+  );
+};
 
-export default App
+export default App;
